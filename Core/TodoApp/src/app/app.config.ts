2981c8d2819e7291c07d469cb076fb5f1f0c5063
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2025-09-22 17:47:22 +0330

    Update .gitignore and untrack .angular/cache files

--- Core/TodoApp/src/app/app.config.ts
+++ Core/TodoApp/src/app/app.config.ts
@@ -7,8 +7,8 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient()
   ]
 };
